14fc0d20b3161e6c3ff0021c9d298cd3a4cd8170
Alejandro de Tovar <venespana159@gmail.com>
2018-11-02 15:04:17 +0100

    Updated service provider and added auto load facades

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace VD;
 
-
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
 class BaseServiceProvider extends EventServiceProvider
@@ -25,16 +25,18 @@ class BaseServiceProvider extends EventServiceProvider
         parent::boot();
         
         $this->loadViewsFrom(__DIR__ . "/../resources/views/adminlte", 'adminlte');
+        $this->loadViewsFrom(__DIR__ . "/../resources/views/grid", 'grid');
+        
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'VD');
 
         $this->publishes([
             __DIR__ . '/../resources/views/adminlte' => resource_path('views/vendor/adminlte'),
+            __DIR__ . '/../resources/views/grid' => resource_path('views/vendor/grid'),
         ], 'views');
 
         $this->publishes([
             __DIR__ . '/../public/adminlte' => public_path('/vendor/adminlte'),
         ], 'assets');
-
     }
 
     /**
@@ -44,5 +46,10 @@ class BaseServiceProvider extends EventServiceProvider
      */
     public function register()
     {
+        //set up facade
+        AliasLoader::getInstance()->alias('Input', 'Illuminate\Support\Facades\Input');
+		AliasLoader::getInstance()->alias('Form', 'Collective\Html\FormFacade');
+		AliasLoader::getInstance()->alias('HTML', 'Collective\Html\HtmlFacade');
+		AliasLoader::getInstance()->alias('Grids', 'Nayjest\Grids\Grids');
     }
 }
